e315ab3c7286295b9619d40656a9739972566e96
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-12-10 10:57:51 -0700

    Adds read-only functionality for users not associated with a snippet

--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .permissions import IsUserOrReadOnly
+from .serializers import SnippetSerializer, UserSerializer
 
 # Create your views here.
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -16,6 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
